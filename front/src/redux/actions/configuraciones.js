32fc49b6573b6f592f9b12585340d19f40b499d4
Pano Valdez <marianovaldez92@protonmail.com>
2024-07-08 20:09:19 -0300

    Cambios de modulos en config

--- front/src/redux/actions/configuraciones.js
+++ front/src/redux/actions/configuraciones.js
@@ -5,16 +5,16 @@ const select = (id) => ({
 
 const get_all = () => async (dispatch) => {
     const payload = [
-      {id: "",full_name: "Area Relaciones Humanas"},
+      {id: "",full_name: "Contactos"},
       {id: "cliente",full_name: "Clientes y Financiadores"},
       {id: "proveedor",full_name: "Proveedores"},
       {id: "",full_name: "Area Economica"},
       {id: "proyecto",full_name: "Proyectos"},
       {id: "caja",full_name: "Tesorería"},
-      {id: "ingreso",full_name: "Ingresos"},
-      {id: "gasto",full_name: "Gastos"},
+      {id: "ingreso",full_name: "Tipos de ingresos"},
+      {id: "gasto",full_name: "Tipos de gastos"},
       {id: "",full_name: "Area Contable"},
-      {id: "titulo",full_name: "Cuentas y titulos contables"},
+      {id: "titulo",full_name: "Cuentas contables"},
     ]
 
     dispatch({
