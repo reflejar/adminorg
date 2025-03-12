2d1a9d4b46559661a92b336ce63d30eb04cf0044
Pano <marianovaldez92@protonmail.com>
2025-03-12 15:49:45 -0300

    arreglo sidebar

--- front/src/components/sidebar.jsx
+++ front/src/components/sidebar.jsx
@@ -1,43 +1,44 @@
 'use client'
 
 import Link from "next/link"
+import Image from "next/image"
 import { usePathname, useRouter } from "next/navigation";
 import {
     UncontrolledDropdown,
     DropdownToggle,
     DropdownMenu,
     DropdownItem,
+    Dropdown
  } from "reactstrap";
 
 import { useAuthContext } from "@/contexts/authContext";
+import { useState } from "react";
 
 export default function Sidebar() {
 
+    const [isOpen, setIsOpen] = useState(false);
     const { changeCommunity, currentUser } = useAuthContext();
+
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+  };
+
     const router = useRouter();
     const pathname = usePathname()
 
     if (!currentUser) return 
 
-    return (<div className="col-lg-2 d-flex flex-column flex-shrink-0 p-3 bg-light min-vh-100">
-                    <Link href="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
-                        <span className="fs-4">{currentUser.logo ? <img src={currentUser.logo} alt={currentUser.community} height={50} /> : currentUser.community}</span>
-                    </Link> 
-                    <hr />
-                    <ul className="nav nav-pills flex-column mb-auto">
-                        <Link href="/reportes" className={`nav-link ${pathname == '/reportes' ? 'active' : 'link-dark'}`}><i className="bi-bar-chart-line me-2" /> Resumen</Link>
-                        <Link href="/cuentas-a-cobrar" className={`nav-link ${pathname == '/cuentas-a-cobrar' ? 'active' : 'link-dark'}`}> <i className="bi-download me-2" /> Cuentas a cobrar</Link>
-                        <Link href="/cuentas-a-pagar" className={`nav-link ${pathname == '/cuentas-a-pagar' ? 'active' : 'link-dark'}`}><i className="bi-upload me-2" /> Cuentas a pagar</Link>
-                        <Link href="/tesoreria" className={`nav-link ${pathname == '/tesoreria' ? 'active' : 'link-dark'}`}><i className="bi-currency-dollar me-2" /> Tesorería</Link>
-                        {currentUser.user.group === "contable" && <Link href="/contabilidad" className={`nav-link ${pathname == '/contabilidad' ? 'active' : 'link-dark'}`}><i className="bi-briefcase me-2" /> Contabilidad</Link>}
-                        <Link href="/configuraciones" className={`nav-link ${pathname == '/configuraciones' ? 'active' : 'link-dark'}`}><i className="bi-house-gear-fill me-2" /> Mi Organización</Link>
-                    </ul>
-                    <hr />
-
-                    <UncontrolledDropdown nav inNavbar className="pr-1" direction="up">
-                        <DropdownToggle nav className="d-flex align-items-center link-dark text-decoration-none dropdown-toggle">
-                            <img src="/img/assistant.svg" alt="" width="32" height="32" className="rounded-circle me-2" />
-                            <strong>{currentUser.profile && currentUser.profile.nombre}</strong>
+    // if mobile make a simple navbar
+    if (window.innerWidth < 768) {
+        return (<div>
+            <nav className="navbar navbar-expand-lg bg-light flex-shrink-0t">
+                <div className="container-fluid">
+                    <Link href="/" className="navbar-brand link-info text-decoration-none">
+                        <Image src={`/img/isoLogo.png`} width={50} height={50} alt="Logo de AdminOrg" /> {currentUser.community}
+                    </Link>
+                    <UncontrolledDropdown  inNavbar className="pr-1" direction="left">
+                        <DropdownToggle nav className="d-flex align-items-center dropdown-toggle">
+                            <img src={`/img/assistant.svg`} alt="" width="32" height="32" className="me-3" />
                         </DropdownToggle>
 
 
@@ -66,20 +67,106 @@ export default function Sidebar() {
                             
                             {/* <Link disabled to="/faq" className="p-0"> */}
                             <DropdownItem disabled>
-                                <i className="bi-bookmark"></i> Biblioteca y FAQ
+                                <i className="bi-info-circle"></i> Ayuda
                             </DropdownItem>
                             {/* </Link> */}
                             <DropdownItem divider />
                             <DropdownItem>
                                 <div onClick={() => router.push('/auth/logout')} >
-                                    <i className="bi-logout"></i> Logout
+                                    <i className="bi-box-arrow-right"></i> Logout
+                                </div>
+                            </DropdownItem>
+                        </DropdownMenu>
+                    </UncontrolledDropdown>                    
+                </div>
+            </nav>
+            <Dropdown isOpen={isOpen} toggle={handleClick} className="m-2" direction="down">
+                <DropdownToggle
+                    color="primary"
+                    className="shadow-sm dropdown-toggle"
+                    block
+                >
+                    {pathname === '/resumen' && <span><i className="bi-bar-chart-line" /> Resumen</span>}
+                    {pathname === '/cuentas-a-cobrar' && <span><i className="bi-download" /> Cuentas a cobrar</span>}
+                    {pathname === '/cuentas-a-pagar' && <span><i className="bi-upload" /> Cuentas a pagar</span>}
+                    {pathname === '/tesoreria' && <span><i className="bi-currency-dollar" /> Tesorería</span>}
+                    {pathname === '/contabilidad' && currentUser.user.group === "contable" && <span><i className="bi-briefcase" /> Contabilidad</span>}
+                    {pathname === '/configuraciones' && <span><i className="bi-house-gear-fill" /> Mi Organización</span>}
+                </DropdownToggle>
+                <DropdownMenu >
+                    <Link href="/resumen" className={`nav-link ${pathname == '/resumen' ? 'active' : 'link-info'}`}><i className="bi-bar-chart-line me-2" /> Resumen</Link>
+                    <Link href="/cuentas-a-cobrar" className={`nav-link ${pathname == '/cuentas-a-cobrar' ? 'active' : 'link-info'}`}> <i className="bi-download me-2" /> Cuentas a cobrar</Link>
+                    <Link href="/cuentas-a-pagar" className={`nav-link ${pathname == '/cuentas-a-pagar' ? 'active' : 'link-info'}`}><i className="bi-upload me-2" /> Cuentas a pagar</Link>
+                    <Link href="/tesoreria" className={`nav-link ${pathname == '/tesoreria' ? 'active' : 'link-info'}`}><i className="bi-currency-dollar me-2" /> Tesorería</Link>
+                    {currentUser.user.group === "contable" && <Link href="/contabilidad" className={`nav-link ${pathname == '/contabilidad' ? 'active' : 'link-info'}`}><i className="bi-briefcase me-2" /> Contabilidad</Link>}
+                    <Link href="/configuraciones" className={`nav-link ${pathname == '/configuraciones' ? 'active' : 'link-info'}`}><i className="bi-house-gear-fill me-2" /> Mi Organización</Link>        
+
+                </DropdownMenu>
+            </Dropdown>           
+        </div>
+        )
+    }
+
+    return (<div className="col-lg-2 d-flex flex-column flex-shrink-0 p-3 bg-light min-vh-100">
+                    <Link href="/" className="d-flex align-items-center mb-md-0 me-md-auto link-info text-decoration-none">
+                        <span className="fs-5">
+                            <Image src={`/img/isoLogo.png`} width={50} height={50} alt="Logo de AdminOrg" /> {currentUser.community}
+                        </span>
+                    </Link> 
+                    <hr />
+                    <ul className="nav nav-pills flex-column mb-auto">
+                        <Link href="/resumen" className={`nav-link ${pathname == '/resumen' ? 'active' : 'link-info'}`}><i className="bi-bar-chart-line me-2" /> Resumen</Link>
+                        <Link href="/cuentas-a-cobrar" className={`nav-link ${pathname == '/cuentas-a-cobrar' ? 'active' : 'link-info'}`}> <i className="bi-download me-2" /> Cuentas a cobrar</Link>
+                        <Link href="/cuentas-a-pagar" className={`nav-link ${pathname == '/cuentas-a-pagar' ? 'active' : 'link-info'}`}><i className="bi-upload me-2" /> Cuentas a pagar</Link>
+                        <Link href="/tesoreria" className={`nav-link ${pathname == '/tesoreria' ? 'active' : 'link-info'}`}><i className="bi-currency-dollar me-2" /> Tesorería</Link>
+                        {currentUser.user.group === "contable" && <Link href="/contabilidad" className={`nav-link ${pathname == '/contabilidad' ? 'active' : 'link-info'}`}><i className="bi-briefcase me-2" /> Contabilidad</Link>}
+                        <Link href="/configuraciones" className={`nav-link ${pathname == '/configuraciones' ? 'active' : 'link-info'}`}><i className="bi-house-gear-fill me-2" /> Mi Organización</Link>
+                    </ul>
+                    <hr />
+
+                    <UncontrolledDropdown inNavbar className="pr-1" direction="up">
+                        <DropdownToggle nav className="d-flex align-items-center link-info text-decoration-none dropdown-toggle">
+                            <img src={`/img/assistant.svg`} alt="" width="32" height="32" className="rounded-circle me-2" />
+                            <strong>{currentUser.profile && currentUser.profile.nombre}</strong>
+                        </DropdownToggle>
+
+
+                        <DropdownMenu>
+                            <DropdownItem disabled>
+                                <span className="text-dark">
+                                {currentUser.profile.nombre}
+                                </span>
+                            </DropdownItem>
+                            <DropdownItem divider />
+
+                            {/* <Link to="/user-profile" className="p-0"> */}
+                            <DropdownItem>
+                                <i className="bi-person"></i> Ver Perfil
+                            </DropdownItem>
+                            {/* </Link> */}
+                            {currentUser.admin_of.length > 0 && <select
+                                name="comunidad"
+                                id="comunidad"
+                                className="form-select my-2"
+                                onChange={(option) => changeCommunity(option.value)}
+                                value={currentUser.community}
+                                >
+                                    {currentUser.admin_of.map((c, k) => (<option key={k} value={c}>{c}</option>))}
+                                </select>}
+                            
+                            {/* <Link disabled to="/faq" className="p-0"> */}
+                            <DropdownItem disabled>
+                                <i className="bi-info-circle"></i> Ayuda 
+                            </DropdownItem>
+                            {/* </Link> */}
+                            <DropdownItem divider />
+                            <DropdownItem>
+                                <div onClick={() => router.push('/auth/logout')} >
+                                    <i className="bi-box-arrow-right"></i> Logout
                                 </div>
                             </DropdownItem>
                         </DropdownMenu>
                     </UncontrolledDropdown>
-
-
-
     </div>
     )
   }
