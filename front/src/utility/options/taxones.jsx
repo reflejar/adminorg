94b38ed2ae4a3831a5f1713c36ca52d770de4a6b
Pano <marianovaldez92@protonmail.com>
2025-03-25 18:38:18 -0300

    utility taxones

--- front/src/utility/options/taxones.jsx
+++ front/src/utility/options/taxones.jsx
@@ -1,7 +1,17 @@
 export const cajas = [
-    { id: null, nombre: "---"},
     { id: 'efectivo', nombre: 'Efectivo' },
     { id: 'banco', nombre: 'Banco' },
     { id: 'billetera', nombre: 'Billetera Virtual' },
     { id: 'cheques', nombre: 'Cheques' },
+];
+
+export const creditos = [
+    { id: 'donantes', nombre: 'Financiador' },
+    { id: 'socios', nombre: 'Socio' },
+    { id: 'clientes', nombre: 'Cliente' },
+];
+
+export const deudas = [
+    { id: 'proveedores', nombre: 'Proveedor' },
+    { id: 'empleados', nombre: 'Empleado' },
 ];
