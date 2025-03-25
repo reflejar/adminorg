8edfc5fef0fb9961ec865dafa2854ae8e8c60a10
Pano <marianovaldez92@protonmail.com>
2025-03-25 18:41:20 -0300

    ajuste de texto

--- front/src/components/CRUD/titulo/_options.js
+++ front/src/components/CRUD/titulo/_options.js
@@ -1,7 +1,7 @@
 export const naturalezas = [
     { id: "creditos", nombre: 'Cuentas a cobrar' },
     { id: "deudas", nombre: 'Cuentas a pagar' },
-    { id: "caja-y-bancos", nombre: 'Caja y Bancos' },
+    { id: "caja-y-bancos", nombre: 'Caja / Bancos' },
     { id: 'ingreso', nombre: 'Ingresos' },
     { id: 'gasto', nombre: 'Gastos' },
 ];    
